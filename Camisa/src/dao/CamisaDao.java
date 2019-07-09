751750877d540126296188d1319ed08f67b6ca61
Administrador <Administrador@10.10.4.23>
2019-07-09 18:40:29 -0300

    top.loose

---
+++ Camisa/src/dao/CamisaDao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Camisa;
+/**
+ *
+ * @author Administrador
+ */
+public class CamisaDao {
+     public static boolean inserir(Camisa objeto) {
+        String sql = "INSERT INTO camisa (codigo, tamanho, cor) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.setString(2, objeto.getTamanho());
+            ps.setString(3, objeto.getCor());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+      public static void main(String[] args) {
+        Camisa objeto = new Camisa();
+        objeto.setCodigo(1);
+        objeto.setTamanho("P");
+        objeto.setCor("Vermelho");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+}
